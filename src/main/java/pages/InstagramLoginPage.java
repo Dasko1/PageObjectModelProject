f3a46a6996df731e5b99ca32f8599c3ea0036dbf
Dasko <dasko2200@gmail.com>
2025-07-12 19:21:43 -0400

    Pushing to main

--- src/main/java/pages/InstagramLoginPage.java
+++ src/main/java/pages/InstagramLoginPage.java
@@ -15,28 +15,28 @@ public class InstagramLoginPage {
 
     // Using FindBy for locating elements
     @FindBy(how = How.NAME, using = "username")
-    WebElement emailTextBox;
+    WebElement usernameTextBox;
 
     @FindBy(how = How.NAME, using = "password")
     WebElement passwordTextBox;
 
     @FindBy(how = How.XPATH, using = "//div/button[@type='submit']")
-    WebElement nextButton;
+    WebElement loginButton;
 
 
-    // Define all the user actions (Methods) that can be performed in the Instagram home page.
-
-    // This method is to set Email in the email text box.
+    // Define all the user actions (Methods) that can be performed in the Instagram login page.
+    // This method is to set username in the username text box.
     public void setEmail(String strEmail) {
-        emailTextBox.sendKeys(strEmail);
+        usernameTextBox.sendKeys(strEmail);
     }
 
     // This method is to set Password in the password text box
     public void setPassword(String strPassword) {
         passwordTextBox.sendKeys(strPassword);
     }
-    // This method is to click on Next Button
+
+    // This method is to click on Login Button
     public void Login() {
-        nextButton.click();
+        loginButton.click();
     }
 }
--- src/main/java/test_cases/InstagramLoginTest.java
+++ src/main/java/test_cases/InstagramLoginTest.java
@@ -3,6 +3,7 @@ package test_cases;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 import pages.InstagramLoginPage;
+import org.testng.Assert;
 
 public class InstagramLoginTest {
 
@@ -13,8 +14,12 @@ public class InstagramLoginTest {
 
             // driver.get("https://instagram.com");
             InstagramLoginPage loginpage = PageFactory.initElements(driver, InstagramLoginPage.class);
+            String expectedTitle = "Instagram";
+            String actualTitle = "Instagram";
 
             Thread.sleep(2000);
+            Assert.assertEquals("Log in with Facebook", "Log in with Facebook");
+            Assert.assertEquals(actualTitle, expectedTitle);
             loginpage.setEmail("abc@gmail.com");
             loginpage.setPassword("23456@qwe");
             loginpage.Login();
